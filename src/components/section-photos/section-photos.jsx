39f6d6b31788c8aa353df89a6a05d41adeb5af72
Katerina <katerina_kw@mail.ru>
2021-01-24 22:44:13 +0300

    Небольшие правки

--- src/components/section-photos/section-photos.jsx
+++ src/components/section-photos/section-photos.jsx
@@ -6,7 +6,7 @@ export const SectionPhotos = () => {
     const selectedDate = useSelector(state => state.data.selectedDate);
 
     const getPhoto = () => {
-        const photos = Array(5).fill(``).map(() => `http://picsum.photos/200/150?r=` + selectedDate + Math.random());
+        const photos = Array(5).fill(``).map(() => `https://picsum.photos/200/150?r=` + selectedDate + Math.random());
         return photos.map((photo, index) => <img style={{height: '150px', width: '210px'}} key={index} className="img-thumbnail" src={photo} alt=""/>);
     }
 
